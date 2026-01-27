a4a33f648b754bf994ccb0f25a56e2acdd35f747
ashira-maharjan <ashiramaharjan13@gmail.com>
2026-01-27 14:30:03 +0545

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,29 @@
-from setuptools import find_packages,setup 
+from setuptools import setup, find_packages
 from typing import List
 
-# create a function for requirements
+HYPEN_E_DOT = '-e .'
 
-HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    Docstring for get_requirements
-    
-    :param file_path: Description
-    :type file_path: str
-    :return: Description
-    :rtype: Any
-
-    this function will return the list of requirements 
+    this function will return the list of requirements
     '''
+
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
-
 setup(
-    name = 'ML_Project',
+    name='Ml_Project',
     version='0.0.1',
-    author='Ashira',
+    description='End_to_End_ML_Project',
+    author='Ashira Maharjan',
     author_email='ashiramaharjan13@gmail.com',
-    package=find_packages(),
-    #install_requires=['pandas','numpy','seaborn'],
+    packages=find_packages(), # Automatically discover and include all packages
+    # install_requires=['pandas', 'numpy', 'seaborn']
     install_requires=get_requirements('requirements.txt')
+    # Other potential arguments: long_description, license, url, entry_points
 )
