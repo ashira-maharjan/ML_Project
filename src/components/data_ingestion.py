2c71ba453ac22781a7fae023ad9fd8633458917f
ashira-maharjan <ashiramaharjan13@gmail.com>
2026-01-28 15:53:18 +0545

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -65,5 +68,11 @@ if __name__ =="__main__":
     train_data, test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    # print r2 score and best model 
+    print(f"Best Model: {(result := modeltrainer.initiate_model_trainer(train_arr, test_arr))[1]} | R2 Score: {result[0]:.4f}")
+    
 
